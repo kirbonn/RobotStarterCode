9fb0f7b82825335be2a10816a68657be611c2f15
kirbon <85096989+kirbonn@users.noreply.github.com>
2025-08-20 17:07:24 +0000

    initial commit

--- Main.java
+++ Main.java
@@ -1,100 +1,89 @@
 import javax.swing.*;
 import java.awt.event.*;
 import kareltherobot.*;
-//This is the starter code for Karel the Robot AP CSA Thai
+import java.util.Scanner;
 
-public class Main implements Directions {
+public class Main implements Directions
+{
+    public static void main(String[] args){
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Please enter your user name");
 
-    public static void turnRight(Robot placeholder) {
-      placeholder.turnLeft();
-      placeholder.turnLeft();
-      placeholder.turnLeft();
-    }
-
-  public static void main(String[] args) 
-	{
-		//My robot's name is karel
-    Robot karel = new Robot(3,1,North,20);
+  String userName = myObj.nextLine();  // Read user input of a String
+  System.out.println("Hello " + userName + "! It is good to meet you!");  // Returns user output user input
+   
+  Scanner in = new Scanner(System.in); // Read the user input of an int
+  System.out.println("How many beepers do you want on each side of the diamond?");
+  int side = in.nextInt(); //Stores the number of beepers on each side of the diamond
+   
+    Robot fred = new Robot(5, 1, North, 10);
+    World.setSize(side*2, side*2);
     World.setVisible(true);
-    World.setSize(10,10);
-    World.setDelay(20);
-
-    // N
-    karel.move();
-    karel.putBeeper();
-    karel.move();
-    karel.putBeeper();
-    karel.move();
-    karel.putBeeper();
-    karel.move();
-    karel.putBeeper();
-
-    karel.turnLeft();
-    karel.turnLeft();
-    karel.turnLeft();
-    karel.move();
-
-    karel.turnLeft();
-    karel.turnLeft();
-    karel.turnLeft();
-    karel.move();
-    karel.putBeeper();
-
-    karel.turnLeft();
-    karel.move();
-
-    turnRight(karel);
-    karel.move();
-    karel.putBeeper();
-
-    karel.turnLeft();
-    karel.move();
-
-    turnRight(karel);
-    karel.move();
-    karel.putBeeper();
-    karel.turnLeft();
-    karel.turnLeft();
-    karel.move();
-    karel.putBeeper();
-    karel.move();
-    karel.putBeeper();
-    karel.move();
-    karel.putBeeper();
-
-    Robot andy = new Robot(3,6,North,20);
-    andy.move();
-    andy.putBeeper();
-    andy.move();
-    andy.putBeeper();
-    andy.move();
-    andy.putBeeper();
-    andy.move();
-    andy.putBeeper();
-    turnRight(andy);
-    andy.move();
-    andy.putBeeper();
-    andy.move();
-    turnRight(andy);
-    andy.move();
-    andy.putBeeper();
-    andy.move();
-    turnRight(andy);
-    andy.move();
-    andy.putBeeper();
-    andy.turnLeft();
-    andy.move();
-    andy.turnLeft();
-    andy.move();
-    andy.putBeeper();
+    World.setDelay(10);
+      leaveFive(fred);
+      fred.turnLeft();
+      fred.turnLeft();
+      leaveAnotherFive(fred);
+  
+    Robot jimmy = new Robot(5, 1, North, 10);
+    leaveAnotherFive(jimmy);
+    leaveAnotherFive(jimmy);
     
-    
-
-
-
-
-
 
   }
 
+    //write a method that will turn right
+public static void turnRight(Robot r) //create a new method using a Robot parameter. r is a temp placeholder.
+    {
+      r.turnLeft();
+      r.turnLeft();
+      r.turnLeft();
+    }
+    //write a method that will go 5 steps and leave beepers each time.
+public static void leaveFive(Robot r) //create a method bringing in a Robot parameter. r is a temp placeholder.
+    {
+     r.putBeeper();
+     turnRight(r);
+     r.move();
+     turnRight(r);
+     r.move();
+     r.putBeeper();
+     r.turnLeft();
+     r.move();
+     turnRight(r);
+     r.move();
+     r.putBeeper();
+     r.turnLeft();
+     r.move();
+     turnRight(r);
+     r.move();
+     r.putBeeper();
+     r.turnLeft();
+     r.move();
+     turnRight(r);
+     r.move();
+     r.putBeeper();
+    }
+
+    public static void leaveAnotherFive(Robot r){
+      r.move();
+      turnRight(r);
+      r.move();
+      r.putBeeper();
+      r.turnLeft();
+      r.move();
+      turnRight(r);
+      r.move();
+      r.putBeeper();
+      r.turnLeft();
+      r.move();
+      turnRight(r);
+      r.move();
+      r.putBeeper();
+      r.turnLeft();
+      r.move();
+      turnRight(r);
+      r.move();
+      r.putBeeper();
+    }
 }
